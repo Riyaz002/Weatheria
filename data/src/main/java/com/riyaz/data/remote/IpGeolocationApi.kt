80087720dbe3601b20771e58b4ccb6b97c7b3415
Riyaz Uddin <riyazps007@gmail.com>
2024-06-11 11:44:33 +0530

    Fixed build error

--- data/src/main/java/com/riyaz/data/remote/IpGeolocationApi.kt
+++ data/src/main/java/com/riyaz/data/remote/IpGeolocationApi.kt
@@ -4,14 +4,16 @@ import com.riyaz.data.BuildConfig
 import com.riyaz.data.remote.model.location.LocationInfoDTO
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.QueryMap
 
 interface IpGeolocationApi {
 
-    @GET("ipgeo?apiKey=$API_KEY")
-    fun getLocationInfo(): Call<LocationInfoDTO>
+    @GET("ipgeo")
+    fun getLocationInfo(
+        @QueryMap query: HashMap<String, String> = hashMapOf("apiKey" to BuildConfig.IP_GEOLOCATION_API_KEY)
+    ): Call<LocationInfoDTO>
 
     companion object{
         const val BASE_URL = "https://api.ipgeolocation.io/"
-        const val API_KEY = BuildConfig.IP_GEOLOCATION_API_KEY
     }
 }
